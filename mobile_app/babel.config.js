9a49a64eb00134a0f5d870c1ae9f15a2183a2a31
Ashmeet Sidhu <ashmeetsidhu04@gmail.com>
2025-09-29 13:32:32 +0530

    [FEATURE] Frontend code refined for AI Analysis Screen and updated the requirements.txt

--- mobile_app/babel.config.js
+++ mobile_app/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  // --- ADD THIS PLUGINS SECTION ---
+  plugins: [
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+    }]
+  ]
 };
